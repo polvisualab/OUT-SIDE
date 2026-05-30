5b241d5fe1db05f4389083d4367a2cfcb83039d7
polvisualab <pol.visualab@gmail.com>
2026-05-30 16:39:45 +0200

    lenis

---
+++ scripts/lenis.js
@@ -0,0 +1,17 @@
+// lenis.js
+import Lenis from "https://cdn.skypack.dev/@studio-freight/lenis";
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smooth: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+export default lenis;
